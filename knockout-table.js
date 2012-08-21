86586c7353d55aaed55e8c97456413737553fd90
Michael Best <mbest@dasya.com>
2012-08-20 15:19:18 -1000

    Initial version

---
+++ knockout-table.js
@@ -0,0 +1,145 @@
+// TABLE BINDING plugin for Knockout http://knockoutjs.com/
+// (c) Michael Best
+// License: MIT (http://www.opensource.org/licenses/mit-license.php)
+// Version 0.1.0
+
+(function(ko, undefined) {
+
+var div = document.createElement('div'),
+    elemTextProp = 'textContent' in div ? 'textContent' : 'innerText';
+div = null;
+
+/*
+ * Table binding
+ */
+ko.bindingHandlers.table = {
+    update: function(element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+        var value = ko.utils.unwrapObservable(valueAccessor()),
+            cols = ko.utils.unwrapObservable(value.columns),
+            rows = ko.utils.unwrapObservable(value.rows),
+            header = value.header,
+            data = value.data,
+            evenClass = value.evenClass,
+            headerIsFunction = typeof header === 'function',
+            numCols = cols && cols.length,
+            numRows = rows && rows.length,
+            itemSubs = [],
+            tableBody;
+
+        // Return the item value and update table cell if observable item changes
+        function unwrapItemAndSubscribe(rowIndex, colIndex) {
+            // Use a data function if provided; otherwise use the column value as a property of the row item
+            var rowItem = rows[rowIndex], colItem = cols[colIndex],
+                itemValue = data ? data(rowItem, colItem) : rowItem[colItem];
+
+            if (ko.isObservable(itemValue)) {
+                itemSubs.push(itemValue.subscribe(function(newValue) {
+                    if (tableBody)
+                        tableBody.rows[rowIndex].cells[colIndex][elemTextProp] = newValue;
+                }));
+                itemValue = itemValue.peek ? itemValue.peek() : ko.ignoreDependencies(itemValue);
+            }
+            return ko.utils.escape(itemValue);
+        }
+
+        // Use header array for number of columnes
+        if (cols === undefined && header && !headerIsFunction) {
+            cols = header.length;
+        }
+
+        // Support either a column array or number
+        if (cols && numCols === undefined) {
+            numCols = cols;
+            cols = ko.utils.range(0, numCols-1);
+        }
+
+        var html = '<table>';
+
+        // Generate a header section if a header function is provided
+        if (header) {
+            html += '<thead><tr>';
+            for (var colIndex = 0; colIndex < numCols; colIndex++) {
+                html += '<th>' + (headerIsFunction ? header(cols[colIndex]) : header[cols[colIndex]]) + '</th>';
+            }
+            html += '</tr></thead>';
+        }
+
+        // Generate the table body section
+        html += '<tbody>';
+        for (var rowIndex = 0; rowIndex < numRows; rowIndex++) {
+            html += (evenClass && !(rowIndex%2)) ? '<tr class="' + evenClass + '">' : '<tr>';
+            for (var colIndex = 0; colIndex < numCols; colIndex++) {
+                html += '<td>' + unwrapItemAndSubscribe(rowIndex, colIndex) + '</td>';
+            }
+            html += '</tr>';
+        }
+        html += '</tbody></table>';
+
+        // Remove previous table contents
+        while (element.firstChild)
+            ko.removeNode(element.firstChild);
+
+        // Insert new table contents
+        var tempDiv = document.createElement('div');
+        tempDiv.innerHTML = html;
+        var tempTable = tempDiv.firstChild;
+        while (tempTable.firstChild)
+            element.appendChild(tempTable.firstChild);
+
+        // Make sure subscriptions are disposed if the table is cleared
+        if (itemSubs) {
+            tableBody = element.tBodies[0];
+            ko.utils.domNodeDisposal.addDisposeCallback(tableBody, function() {
+                ko.utils.arrayForEach(itemSubs, function(itemSub) {
+                    itemSub.dispose();
+                });
+            });
+        }
+    }
+};
+
+/*
+ * Escape a string for html representation
+ */
+ko.utils.escape = function(string) {
+    return (''+string).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, '&#x27;').replace(/\//g,'&#x2F;');
+};
+
+/*
+ * Helper functions for finding minified property names
+ */
+function findNameMethodSignatureContaining(obj, match) {
+    for (var a in obj)
+        if (obj.hasOwnProperty(a) && obj[a].toString().indexOf(match) >= 0)
+            return a;
+}
+
+function findPropertyName(obj, equals) {
+    for (var a in obj)
+        if (obj.hasOwnProperty(a) && obj[a] === equals)
+            return a;
+}
+
+function findSubObjectWithProperty(obj, prop) {
+    for (var a in obj)
+        if (obj.hasOwnProperty(a) && obj[a] && obj[a][prop])
+            return obj[a];
+}
+
+/*
+ * ko.ignoreDependencies is used to access observables without creating a dependency
+ */
+if (!ko.ignoreDependencies) {
+    var depDet = findSubObjectWithProperty(ko, 'end'),
+        depDetBeginName = findNameMethodSignatureContaining(depDet, '.push({');
+    ko.ignoreDependencies = function(callback, object, args) {
+        try {
+            depDet[depDetBeginName](function() {});
+            return callback.apply(object, args || []);
+        } finally {
+            depDet.end();
+        }
+    }
+}
+
+})(ko);
---
+++ spec/tableBinding.js
@@ -0,0 +1,225 @@
+describe('table binding', {
+    before_each: function () {
+        var existingNode = document.getElementById("testNode");
+        if (existingNode != null)
+            existingNode.parentNode.removeChild(existingNode);
+        testNode = document.createElement("div");
+        testNode.id = "testNode";
+        document.body.appendChild(testNode);
+    },
+
+    'Should generate a table of items from an array of row arrays and a column count': function() {
+        testNode.innerHTML = "<table data-bind=\"table: {rows: rows, columns: 3}\"></table>";
+        var vm = {
+            rows: [
+                [ 1, 2, 3 ],
+                [ 4, 5, 6 ],
+                [ 7, 8, 9 ]
+            ]
+        };
+        ko.applyBindings(vm, testNode);
+        value_of(testNode).should_contain_text('123456789');
+        value_of(testNode.childNodes[0]).should_contain_html(
+            '<tbody>'+
+                '<tr><td>1</td><td>2</td><td>3</td></tr>'+
+                '<tr><td>4</td><td>5</td><td>6</td></tr>'+
+                '<tr><td>7</td><td>8</td><td>9</td></tr>'+
+            '</tbody>');
+    },
+
+    'Should generate a header row if a header function is provided': function() {
+        testNode.innerHTML = "<table data-bind=\"table: {rows: rows, columns: 3, header: function(column) { return columnHeaders[column] } }\"></table>";
+        var vm = {
+            rows: [
+                [ 1, 2, 3 ],
+                [ 4, 5, 6 ],
+                [ 7, 8, 9 ]
+            ],
+            columnHeaders: [ 'a', 'b', 'c' ]
+        };
+        ko.applyBindings(vm, testNode);
+        value_of(testNode).should_contain_text('abc123456789');
+        value_of(testNode.childNodes[0]).should_contain_html(
+            '<thead><tr><th>a</th><th>b</th><th>c</th></tr></thead>' +
+            '<tbody>'+
+                '<tr><td>1</td><td>2</td><td>3</td></tr>'+
+                '<tr><td>4</td><td>5</td><td>6</td></tr>'+
+                '<tr><td>7</td><td>8</td><td>9</td></tr>'+
+            '</tbody>');
+    },
+
+    'Should use a data function, if provided, to get the table item values': function() {
+        testNode.innerHTML = "<table data-bind=\"table: {rows: rows, columns: 3, data: function(row, col) { return 'x'+row[col] } }\"></table>";
+        var vm = {
+            rows: [
+                [ 1, 2, 3 ],
+                [ 4, 5, 6 ],
+                [ 7, 8, 9 ]
+            ]
+        };
+        ko.applyBindings(vm, testNode);
+        value_of(testNode).should_contain_text('x1x2x3x4x5x6x7x8x9');
+        value_of(testNode.childNodes[0]).should_contain_html(
+            '<tbody>'+
+                '<tr><td>x1</td><td>x2</td><td>x3</td></tr>'+
+                '<tr><td>x4</td><td>x5</td><td>x6</td></tr>'+
+                '<tr><td>x7</td><td>x8</td><td>x9</td></tr>'+
+            '</tbody>');
+    },
+
+    'Should generate a header row if a header array is provided': function() {
+        testNode.innerHTML = "<table data-bind=\"table: {rows: rows, header: columnHeaders }\"></table>";
+        var vm = {
+            rows: [
+                [ 1, 2, 3 ],
+                [ 4, 5, 6 ],
+                [ 7, 8, 9 ]
+            ],
+            columnHeaders: [ 'a', 'b', 'c' ]
+        };
+        ko.applyBindings(vm, testNode);
+        value_of(testNode).should_contain_text('abc123456789');
+        value_of(testNode.childNodes[0]).should_contain_html(
+            '<thead><tr><th>a</th><th>b</th><th>c</th></tr></thead>' +
+            '<tbody>'+
+                '<tr><td>1</td><td>2</td><td>3</td></tr>'+
+                '<tr><td>4</td><td>5</td><td>6</td></tr>'+
+                '<tr><td>7</td><td>8</td><td>9</td></tr>'+
+            '</tbody>');
+    },
+
+    'Should generate a table of items from an array of row objects and and array of columns': function() {
+        testNode.innerHTML = "<table data-bind=\"table: {rows: rows, columns: columns}\"></table>";
+        var vm = {
+            rows: [
+                { col1: 1, col2: 2, col3: 3 },
+                { col1: 4, col2: 5, col3: 6 },
+                { col1: 7, col2: 8, col3: 9 }
+            ],
+            columns: [ 'col1', 'col2', 'col3' ]
+        };
+        ko.applyBindings(vm, testNode);
+        value_of(testNode).should_contain_text('123456789');
+        value_of(testNode.childNodes[0]).should_contain_html(
+            '<tbody>'+
+                '<tr><td>1</td><td>2</td><td>3</td></tr>'+
+                '<tr><td>4</td><td>5</td><td>6</td></tr>'+
+                '<tr><td>7</td><td>8</td><td>9</td></tr>'+
+            '</tbody>');
+    },
+
+    'Should generate a table of items from an array of row objects and and array of columns objects using a data function': function() {
+        testNode.innerHTML = "<table data-bind=\"table: {rows: rows, columns: columns, data: function(row, col) {return row[col.prop]} }\"></table>";
+        var vm = {
+            rows: [
+                { col1: 1, col2: 2, col3: 3 },
+                { col1: 4, col2: 5, col3: 6 },
+                { col1: 7, col2: 8, col3: 9 }
+            ],
+            columns: [ {prop: 'col1'}, {prop: 'col2'}, {prop: 'col3'} ]
+        };
+        ko.applyBindings(vm, testNode);
+        value_of(testNode).should_contain_text('123456789');
+        value_of(testNode.childNodes[0]).should_contain_html(
+            '<tbody>'+
+                '<tr><td>1</td><td>2</td><td>3</td></tr>'+
+                '<tr><td>4</td><td>5</td><td>6</td></tr>'+
+                '<tr><td>7</td><td>8</td><td>9</td></tr>'+
+            '</tbody>');
+    },
+
+    'Should set the class for even rows if evenClass is set': function() {
+        testNode.innerHTML = "<table data-bind=\"table: {rows: rows, columns: 3, evenClass: 'even'}\"></table>";
+        var vm = {
+            rows: [
+                [ 1, 2, 3 ],
+                [ 4, 5, 6 ],
+                [ 7, 8, 9 ]
+            ]
+        };
+        ko.applyBindings(vm, testNode);
+        value_of(testNode).should_contain_text('123456789');
+        value_of(testNode.childNodes[0]).should_contain_html(
+            '<tbody>'+
+                '<tr class="even"><td>1</td><td>2</td><td>3</td></tr>'+
+                '<tr><td>4</td><td>5</td><td>6</td></tr>'+
+                '<tr class="even"><td>7</td><td>8</td><td>9</td></tr>'+
+            '</tbody>');
+    },
+
+    'Should re-generate table if an observable row array is updated or an observable column value is updated': function() {
+        testNode.innerHTML = "<table data-bind=\"table: {rows: rows, columns: columns}\"></table>";
+        var vm = {
+            rows: ko.observableArray([
+                [ 1, 2, 3 ],
+                [ 4, 5, 6 ],
+                [ 7, 8, 9 ]
+            ]),
+            columns: ko.observable(3)
+        };
+        ko.applyBindings(vm, testNode);
+        value_of(testNode).should_contain_text('123456789');
+        value_of(testNode.childNodes[0]).should_contain_html(
+            '<tbody>'+
+                '<tr><td>1</td><td>2</td><td>3</td></tr>'+
+                '<tr><td>4</td><td>5</td><td>6</td></tr>'+
+                '<tr><td>7</td><td>8</td><td>9</td></tr>'+
+            '</tbody>');
+
+        // add a row
+        vm.rows.push([ 'x', 'y', 'z' ]);
+        value_of(testNode).should_contain_text('123456789xyz');
+        value_of(testNode.childNodes[0]).should_contain_html(
+            '<tbody>'+
+                '<tr><td>1</td><td>2</td><td>3</td></tr>'+
+                '<tr><td>4</td><td>5</td><td>6</td></tr>'+
+                '<tr><td>7</td><td>8</td><td>9</td></tr>'+
+                '<tr><td>x</td><td>y</td><td>z</td></tr>'+
+            '</tbody>');
+
+        // remove a column
+        vm.columns(2);
+        value_of(testNode).should_contain_text('124578xy');
+        value_of(testNode.childNodes[0]).should_contain_html(
+            '<tbody>'+
+                '<tr><td>1</td><td>2</td></tr>'+
+                '<tr><td>4</td><td>5</td></tr>'+
+                '<tr><td>7</td><td>8</td></tr>'+
+                '<tr><td>x</td><td>y</td></tr>'+
+            '</tbody>');
+    },
+
+    'Should update cell items if an observable is updated (and not regenerate whole table)': function() {
+        testNode.innerHTML = "<table data-bind=\"table: {rows: rows, columns: 3}\"></table>";
+        var vm = {
+            rows: [
+                [ ko.observable(1), 2, 3 ],
+                [ ko.observable(4), 5, 6 ],
+                [ ko.observable(7), 8, 9 ]
+            ]
+        };
+        ko.applyBindings(vm, testNode);
+        value_of(testNode).should_contain_text('123456789');
+        value_of(testNode.childNodes[0]).should_contain_html(
+            '<tbody>'+
+                '<tr><td>1</td><td>2</td><td>3</td></tr>'+
+                '<tr><td>4</td><td>5</td><td>6</td></tr>'+
+                '<tr><td>7</td><td>8</td><td>9</td></tr>'+
+            '</tbody>');
+
+        // set a class for the tbody to show that it doesn't get regenerated
+        testNode.childNodes[0].tBodies[0].className = "test";
+
+        // update three items
+        vm.rows[0][0](10);
+        vm.rows[1][0](40);
+        vm.rows[2][0](70);
+        value_of(testNode).should_contain_text('102340567089');
+        value_of(testNode.childNodes[0]).should_contain_html(
+            '<tbody class="test">'+
+                '<tr><td>10</td><td>2</td><td>3</td></tr>'+
+                '<tr><td>40</td><td>5</td><td>6</td></tr>'+
+                '<tr><td>70</td><td>8</td><td>9</td></tr>'+
+            '</tbody>');
+    }
+});
